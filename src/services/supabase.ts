bbc6c703a1f315791f1f132ab78631fca97d34f2
shayan heidary <hdrydeveloper@gmail.com>
2025-01-31 21:13:37 +0100

    add supabase client

---
+++ src/services/supabase.ts
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vaqclgxizurolsmejiol.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcWNsZ3hpenVyb2xzbWVqaW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzNDk2MTUsImV4cCI6MjA1MzkyNTYxNX0.bTQFRG2uDg14eBMD61EmUwGyprN0zR4R0ukNxjrufos";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
