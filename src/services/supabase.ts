8e9cca4e6129c5805935eb9e7cd9202d39544f6c
shayan heidary <hdrydeveloper@gmail.com>
2025-01-31 21:21:23 +0100

    add Database type

--- src/services/supabase.ts
+++ src/services/supabase.ts
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "../../database.types";
 const supabaseUrl = "https://vaqclgxizurolsmejiol.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZhcWNsZ3hpenVyb2xzbWVqaW9sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzNDk2MTUsImV4cCI6MjA1MzkyNTYxNX0.bTQFRG2uDg14eBMD61EmUwGyprN0zR4R0ukNxjrufos";
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient<Database>(supabaseUrl, supabaseKey);
 
 export default supabase;
