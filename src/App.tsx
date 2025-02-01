267d793812fa91ff9bc3838255dd6b9830741930
shayan heidary <hdrydeveloper@gmail.com>
2025-02-01 14:23:50 +0100

    add mutation - delete cabin - toasts - container to layout

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 const queryCleint = new QueryClient({
   defaultOptions: {
@@ -39,6 +40,26 @@ function App() {
         </Routes>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
+      <Toaster
+        position="top-center"
+        gutter={18}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "15px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- src/features/cabins/CabinRow.tsx
+++ src/features/cabins/CabinRow.tsx
@@ -1,12 +1,16 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteCabin } from "../../services/apiCabins";
+import Spinner from "../../ui/Spinner";
+import toast from "react-hot-toast";
 
 const TableRow = styled.div`
   display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
   align-items: center;
-  padding: 1.4rem 2.4rem;
+  padding: 1.4rem 3.4rem;
 
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
@@ -16,6 +20,7 @@ const TableRow = styled.div`
 const Img = styled.img`
   display: block;
   width: 6.4rem;
+  min-width: 3rem;
   aspect-ratio: 3 / 2;
   object-fit: cover;
   object-position: center;
@@ -54,7 +59,25 @@ export default function CabinRow({
     regularPrice: number | null;
   };
 }) {
-  const { image, name, maxCapacity, regularPrice, discount } = cabin;
+  const { image, name, maxCapacity, regularPrice, discount, id } = cabin;
+
+  const queryQlient = useQueryClient();
+
+  const { isPending, mutate } = useMutation({
+    mutationFn: (id: number) => deleteCabin(id),
+    onSuccess: () => {
+      toast.success("cabin deleted successfully");
+      queryQlient.invalidateQueries({
+        queryKey: ["cabin"],
+      });
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+
+  if (isPending) return <Spinner />;
+
   return (
     <TableRow role="row">
       <Img src={image as string} />
@@ -62,7 +85,7 @@ export default function CabinRow({
       <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice as number)}</Price>
       <Discount>{formatCurrency(discount as number)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(id)}>Delete</button>
     </TableRow>
   );
 }
