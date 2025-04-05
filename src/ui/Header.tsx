e2b3578843b39c13d2697847d464bb9327053fa4
shayan heidary <hdrydeveloper@gmail.com>
2025-04-05 16:24:28 +0200

    add signup form - react-form - validation - error

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Logout from "../features/authentication/Logout";
+
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
