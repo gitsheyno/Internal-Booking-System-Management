a2cee1de30f577ff0cb05b786f33b05de443b8cb
shayan heidary <hdrydeveloper@gmail.com>
2025-04-06 15:10:17 +0200

    add HeaderMenu Component

--- src/features/authentication/UserAvatar.tsx
+++ src/features/authentication/UserAvatar.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { useUser } from "./useUserHook";
 
 const StyledUserAvatar = styled.div`
   display: flex;
@@ -19,3 +20,21 @@ const Avatar = styled.img`
   border-radius: 50%;
   outline: 2px solid var(--color-grey-100);
 `;
+
+export default function UserAvatar() {
+  const { user } = useUser();
+
+  console.log("user", user);
+
+  if (!user || !user.user_metadata) {
+    return <div>No user data available</div>;
+  }
+
+  const { fullName, avatar } = user.user_metadata;
+  return (
+    <StyledUserAvatar>
+      <Avatar src={avatar || "default-user.jpg"} alt={`${fullName}'s avatar`} />
+      <span>{fullName}</span>
+    </StyledUserAvatar>
+  );
+}
