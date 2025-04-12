4c09473999b6388f2dd26127f2ef3cb5430053a3
shayan heidary <hdrydeveloper@gmail.com>
2025-04-12 21:38:23 +0200

    add darkmode - toggle component

---
+++ src/ui/DarkModeToggle.tsx
@@ -0,0 +1,9 @@
+import { HiOutlineMoon } from "react-icons/hi2";
+import ButtonIcon from "./ButtonIcon";
+export default function DarkModeToggle() {
+  return (
+    <ButtonIcon aria-label="Toggle dark mode">
+      <HiOutlineMoon />
+    </ButtonIcon>
+  );
+}
